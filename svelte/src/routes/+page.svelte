9f773db6328f28b6e7838ede5a90ff0b58a2bf2c
Zachary Kraft <creativeautomaton@users.noreply.github.com>
2022-10-18 14:11:30 -0500

    latest

---
+++ svelte/src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+  export let name;
+
+  // import any components
+  import { Button, Checkbox } from "svelte-mui";
+
+  let checked = true;
+</script>
+
+<h1>Hello {name}!</h1>
+
+<Checkbox bind:checked>Checkbox</Checkbox>
+
+<p>Checkbox is <strong>{checked ? "checked" : "unchecked"}</strong></p>
+
+<Button
+  outlined
+  shaped
+  color="Red"
+  on:click={() => {
+    checked = !checked;
+  }}
+>
+  Inverse
+</Button>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
